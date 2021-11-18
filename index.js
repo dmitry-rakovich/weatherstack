646d7f51e86fac155ac7c16761849cc566bcd0ae
Dmitry Rakovich <86928979+dmitry-rakovich@users.noreply.github.com>
2021-11-19 00:20:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+const city = document.querySelector('.city');
+const degree = document.querySelector('.degree');
+const country = document.querySelector('.country');
+const time = document.querySelector('.time')
+
+function getWeather() {
+  if (input.value !== '' && isNaN(Number(input.value))) {
+    fetch(
+      `http://api.weatherstack.com/current?access_key=98d80252ba98f95845714563be3d9a86&query=${input.value},${country.value}`
+    )
+      .then((res) => res.json())
+      .then(({ current, location }) => {
+        (degree.innerText = `${current.temperature} \u00b0C`),
+          (city.innerText = `${location.name}, ${location.country}`),
+          (time.innerText = (location.localtime));
+      });
+  }
+}
+
+button.addEventListener('click', getWeather);
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    getWeather();
+  }
+});
